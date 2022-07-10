ae264f40b22db9eb972a0842e2c6ebf7fef9b6eb
ezathashim <ezathashim@mac.com>
2022-07-10 13:19:31 -0400

    Added API to find visible EventDescriptors and scrolling to a specific event

--- Sources/DayView.swift
+++ Sources/DayView.swift
@@ -1,236 +1,261 @@
 import UIKit
 
 @objc public protocol DayViewDelegate: AnyObject {
-  func dayViewDidChangeHeightScaleFactor(dayView: DayView)
-  func dayViewDidSelectEventView(_ eventView: EventView)
-  func dayViewDidLongPressEventView(_ eventView: EventView)
-  func dayView(dayView: DayView, didTapTimelineAt date: Date)
-  func dayView(dayView: DayView, didLongPressTimelineAt date: Date)
-  func dayViewDidBeginDragging(dayView: DayView)
-  func dayViewDidTransitionCancel(dayView: DayView)
-  func dayView(dayView: DayView, willMoveTo date: Date)
-  func dayView(dayView: DayView, didMoveTo  date: Date)
-  func dayView(dayView: DayView, didUpdate event: EventDescriptor)
-  func openIntervalForDate(_ date: Date) -> NSDateInterval
+    func dayViewDidChangeHeightScaleFactor(dayView: DayView)
+    func dayViewDidSelectEventView(_ eventView: EventView)
+    func dayViewDidLongPressEventView(_ eventView: EventView)
+    func dayView(dayView: DayView, didTapTimelineAt date: Date)
+    func dayView(dayView: DayView, didLongPressTimelineAt date: Date)
+    func dayViewDidBeginDragging(dayView: DayView)
+    func dayViewDidTransitionCancel(dayView: DayView)
+    func dayView(dayView: DayView, willMoveTo date: Date)
+    func dayView(dayView: DayView, didMoveTo  date: Date)
+    func dayView(dayView: DayView, didUpdate event: EventDescriptor)
+    func openIntervalForDate(_ date: Date) -> NSDateInterval
 }
 
 public class DayView: UIView, TimelinePagerViewDelegate {
-  public weak var dataSource: EventDataSource? {
-    get {
-      timelinePagerView.dataSource
+    public weak var dataSource: EventDataSource? {
+        get {
+            timelinePagerView.dataSource
+        }
+        set(value) {
+            timelinePagerView.dataSource = value
+        }
     }
-    set(value) {
-      timelinePagerView.dataSource = value
-    }
-  }
-
-  public weak var delegate: DayViewDelegate?
-
-  /// Hides or shows header view
-  public var isHeaderViewVisible = true {
-    didSet {
-      headerHeight = isHeaderViewVisible ? DayView.headerVisibleHeight : 0
-      dayHeaderView.isHidden = !isHeaderViewVisible
-      setNeedsLayout()
-      configureLayout()
-    }
-  }
-
-  public var timelineScrollOffset: CGPoint {
-    timelinePagerView.timelineScrollOffset
-  }
-
-  private static let headerVisibleHeight: CGFloat = 88
-  public var headerHeight: CGFloat = headerVisibleHeight
-
-  @objc public var autoScrollToFirstEvent: Bool {
-    get {
-      timelinePagerView.autoScrollToFirstEvent
-    }
-    set (value) {
-      timelinePagerView.autoScrollToFirstEvent = value
-    }
-  }
-
-  public let dayHeaderView: DayHeaderView
-  public let timelinePagerView: TimelinePagerView
-
-  @objc public var heightScaleFactor: CGFloat{
-      set { timelinePagerView.heightScaleFactor = newValue}
-      get { timelinePagerView.heightScaleFactor }
-  }
-
-  public var state: DayViewState? {
-    didSet {
-      dayHeaderView.state = state
-      timelinePagerView.state = state
-    }
-  }
-
-  public var calendar: Calendar = Calendar.autoupdatingCurrent
-
-  public var eventEditingSnappingBehavior: EventEditingSnappingBehavior {
-    get {
-      timelinePagerView.eventEditingSnappingBehavior
-    }
-    set {
-      timelinePagerView.eventEditingSnappingBehavior = newValue
-    }
-  }
-
-  private var style = CalendarStyle()
-
-  public init(calendar: Calendar = Calendar.autoupdatingCurrent) {
-    self.calendar = calendar
-    self.dayHeaderView = DayHeaderView(calendar: calendar)
-    self.timelinePagerView = TimelinePagerView(calendar: calendar)
-    super.init(frame: .zero)
-    configure()
-  }
-
-  override public init(frame: CGRect) {
-    self.dayHeaderView = DayHeaderView(calendar: calendar)
-    self.timelinePagerView = TimelinePagerView(calendar: calendar)
-    super.init(frame: frame)
-    configure()
-  }
-
-  required public init?(coder aDecoder: NSCoder) {
-    self.dayHeaderView = DayHeaderView(calendar: calendar)
-    self.timelinePagerView = TimelinePagerView(calendar: calendar)
-    super.init(coder: aDecoder)
-    configure()
-  }
-
-  private func configure() {
-    addSubview(timelinePagerView)
-    addSubview(dayHeaderView)
-    configureLayout()
-    timelinePagerView.delegate = self
-
-    if state == nil {
-      let newState = DayViewState(date: Date(), calendar: calendar)
-      newState.move(to: Date())
-      state = newState
-    }
-  }
-  
-  private func configureLayout() {
-    if #available(iOS 11.0, *) {
-      dayHeaderView.translatesAutoresizingMaskIntoConstraints = false
-      timelinePagerView.translatesAutoresizingMaskIntoConstraints = false
-      
-      dayHeaderView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-      dayHeaderView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-      dayHeaderView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
-      let heightConstraint = dayHeaderView.heightAnchor.constraint(equalToConstant: headerHeight)
-      heightConstraint.priority = .defaultLow
-      heightConstraint.isActive = true
-      
-      timelinePagerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-      timelinePagerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-      timelinePagerView.topAnchor.constraint(equalTo: dayHeaderView.bottomAnchor).isActive = true
-      timelinePagerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-    }
-  }
-
-  public func updateStyle(_ newStyle: CalendarStyle) {
-    style = newStyle
-    dayHeaderView.updateStyle(style.header)
-    timelinePagerView.updateStyle(style.timeline)
-  }
-
-  public func timelinePanGestureRequire(toFail gesture: UIGestureRecognizer) {
-    timelinePagerView.timelinePanGestureRequire(toFail: gesture)
-  }
-
-  @objc public func scrollTo(hour24: Float, animated: Bool = true) {
-    timelinePagerView.scrollTo(hour24: hour24, animated: animated)
-  }
-
-  @objc public func scrollToFirstEventIfNeeded(animated: Bool = true) {
-    timelinePagerView.scrollToFirstEventIfNeeded(animated: animated)
-  }
-
-  public func reloadData() {
-    timelinePagerView.reloadData()
-  }
-  
-  public func move(to date: Date) {
-    state?.move(to: date)
-  }
-
-  override public func layoutSubviews() {
-    super.layoutSubviews()
-    if #available(iOS 11, *) {} else {
-      dayHeaderView.frame = CGRect(origin: CGPoint(x: 0, y: layoutMargins.top),
-                                   size: CGSize(width: bounds.width, height: headerHeight))
-      let timelinePagerHeight = bounds.height - dayHeaderView.frame.maxY
-      timelinePagerView.frame = CGRect(origin: CGPoint(x: 0, y: dayHeaderView.frame.maxY),
-                                       size: CGSize(width: bounds.width, height: timelinePagerHeight))
-    }
-  }
-
-  public func transitionToHorizontalSizeClass(_ sizeClass: UIUserInterfaceSizeClass) {
-    dayHeaderView.transitionToHorizontalSizeClass(sizeClass)
-    updateStyle(style)
-  }
-
-  public func create(event: EventDescriptor, animated: Bool = false) {
-    timelinePagerView.create(event: event, animated: animated)
-  }
-
-  public func beginEditing(event: EventDescriptor, animated: Bool = false) {
-    timelinePagerView.beginEditing(event: event, animated: animated)
-  }
-  
-  public func endEventEditing() {
-    timelinePagerView.endEventEditing()
-  }
-
-  // MARK: TimelinePagerViewDelegate
-
     
-  public func timelinePagerDidChangeHeightScaleFactor(timelinePager: TimelinePagerView) {
-      delegate?.dayViewDidChangeHeightScaleFactor(dayView: self)
-  }
+    public weak var delegate: DayViewDelegate?
     
-  public func timelinePagerDidSelectEventView(_ eventView: EventView) {
-    delegate?.dayViewDidSelectEventView(eventView)
-  }
-  public func timelinePagerDidLongPressEventView(_ eventView: EventView) {
-    delegate?.dayViewDidLongPressEventView(eventView)
-  }
-  public func timelinePagerDidBeginDragging(timelinePager: TimelinePagerView) {
-    delegate?.dayViewDidBeginDragging(dayView: self)
-  }
-  public func timelinePagerDidTransitionCancel(timelinePager: TimelinePagerView) {
-    delegate?.dayViewDidTransitionCancel(dayView: self)
-  }
-  public func timelinePager(timelinePager: TimelinePagerView, willMoveTo date: Date) {
-    delegate?.dayView(dayView: self, willMoveTo: date)
-  }
-  public func timelinePager(timelinePager: TimelinePagerView, didMoveTo  date: Date) {
-    delegate?.dayView(dayView: self, didMoveTo: date)
-  }
-  public func timelinePager(timelinePager: TimelinePagerView, didLongPressTimelineAt date: Date) {
-    delegate?.dayView(dayView: self, didLongPressTimelineAt: date)
-  }
-  public func timelinePager(timelinePager: TimelinePagerView, didTapTimelineAt date: Date) {
-    delegate?.dayView(dayView: self, didTapTimelineAt: date)
-  }
-  public func timelinePager(timelinePager: TimelinePagerView, didUpdate event: EventDescriptor) {
-    delegate?.dayView(dayView: self, didUpdate: event)
-  }
+        /// Hides or shows header view
+    public var isHeaderViewVisible = true {
+        didSet {
+            headerHeight = isHeaderViewVisible ? DayView.headerVisibleHeight : 0
+            dayHeaderView.isHidden = !isHeaderViewVisible
+            setNeedsLayout()
+            configureLayout()
+        }
+    }
     
-  public func openIntervalForDate(_ date: Date) -> NSDateInterval {
+    public var timelineScrollOffset: CGPoint {
+        timelinePagerView.timelineScrollOffset
+    }
+    
+    private static let headerVisibleHeight: CGFloat = 88
+    public var headerHeight: CGFloat = headerVisibleHeight
+    
+    @objc public var autoScrollToFirstEvent: Bool {
+        get {
+            timelinePagerView.autoScrollToFirstEvent
+        }
+        set (value) {
+            timelinePagerView.autoScrollToFirstEvent = value
+        }
+    }
+    
+    public let dayHeaderView: DayHeaderView
+    public let timelinePagerView: TimelinePagerView
+    
+    @objc public var heightScaleFactor: CGFloat{
+        set { timelinePagerView.heightScaleFactor = newValue}
+        get { timelinePagerView.heightScaleFactor }
+    }
+    
+    public var state: DayViewState? {
+        didSet {
+            dayHeaderView.state = state
+            timelinePagerView.state = state
+        }
+    }
+    
+    public var calendar: Calendar = Calendar.autoupdatingCurrent
+    
+    public var eventEditingSnappingBehavior: EventEditingSnappingBehavior {
+        get {
+            timelinePagerView.eventEditingSnappingBehavior
+        }
+        set {
+            timelinePagerView.eventEditingSnappingBehavior = newValue
+        }
+    }
+    
+    private var style = CalendarStyle()
+    
+    public init(calendar: Calendar = Calendar.autoupdatingCurrent) {
+        self.calendar = calendar
+        self.dayHeaderView = DayHeaderView(calendar: calendar)
+        self.timelinePagerView = TimelinePagerView(calendar: calendar)
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    override public init(frame: CGRect) {
+        self.dayHeaderView = DayHeaderView(calendar: calendar)
+        self.timelinePagerView = TimelinePagerView(calendar: calendar)
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        self.dayHeaderView = DayHeaderView(calendar: calendar)
+        self.timelinePagerView = TimelinePagerView(calendar: calendar)
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    private func configure() {
+        addSubview(timelinePagerView)
+        addSubview(dayHeaderView)
+        configureLayout()
+        timelinePagerView.delegate = self
         
-      let startOfToday = NSCalendar.current.startOfDay(for: date)
-      let endOfToday = Date(timeInterval: 86399, since: startOfToday)
+        if state == nil {
+            let newState = DayViewState(date: Date(), calendar: calendar)
+            newState.move(to: Date())
+            state = newState
+        }
+    }
+    
+    private func configureLayout() {
+        if #available(iOS 11.0, *) {
+            dayHeaderView.translatesAutoresizingMaskIntoConstraints = false
+            timelinePagerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            dayHeaderView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+            dayHeaderView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+            dayHeaderView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+            let heightConstraint = dayHeaderView.heightAnchor.constraint(equalToConstant: headerHeight)
+            heightConstraint.priority = .defaultLow
+            heightConstraint.isActive = true
+            
+            timelinePagerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+            timelinePagerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+            timelinePagerView.topAnchor.constraint(equalTo: dayHeaderView.bottomAnchor).isActive = true
+            timelinePagerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        }
+    }
+    
+    public func updateStyle(_ newStyle: CalendarStyle) {
+        style = newStyle
+        dayHeaderView.updateStyle(style.header)
+        timelinePagerView.updateStyle(style.timeline)
+    }
+    
+    public func timelinePanGestureRequire(toFail gesture: UIGestureRecognizer) {
+        timelinePagerView.timelinePanGestureRequire(toFail: gesture)
+    }
+    
+    @objc public func scrollTo(hour24: Float, animated: Bool = true) {
+        timelinePagerView.scrollTo(hour24: hour24, animated: animated)
+    }
+    
+    
+    @objc public func scrollToFirstEventIfNeeded(animated: Bool = true) {
+        timelinePagerView.scrollToFirstEventIfNeeded(animated: animated)
+    }
+    
+    @objc public func scrollToFirstEvent(animated: Bool = true) {
+        timelinePagerView.scrollToFirstEvent(animated: animated)
+    }
+    
+    @objc public var firstEvent : EventDescriptor? {
+        return timelinePagerView.firstEvent
+    }
+    
+    @objc public func visibleDescriptors(heightFraction: CGFloat = 1.0) -> [EventDescriptor]? {
+        return timelinePagerView.visibleDescriptors(heightFraction: heightFraction)
+    }
+    
+    @objc public func scrollTo(event: EventDescriptor?, animated: Bool) {
+        timelinePagerView.scrollTo(event: event, animated: animated)
+    }
+    
+    @objc public func scrollTo(hour24: Float, minute: Float, animated: Bool = true) {
+        timelinePagerView.scrollTo(hour24: hour24, minute: minute, animated: animated)
+    }
+    
+    @objc public func scrollToNowTime(animated: Bool = true) {
+        timelinePagerView.scrollToNowTime(animated: animated)
+    }
+    
+    public func reloadData() {
+        timelinePagerView.reloadData()
+    }
+    
+    @objc public func move(to date: Date) {
+        state?.move(to: date)
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        if #available(iOS 11, *) {} else {
+            dayHeaderView.frame = CGRect(origin: CGPoint(x: 0, y: layoutMargins.top),
+                                         size: CGSize(width: bounds.width, height: headerHeight))
+            let timelinePagerHeight = bounds.height - dayHeaderView.frame.maxY
+            timelinePagerView.frame = CGRect(origin: CGPoint(x: 0, y: dayHeaderView.frame.maxY),
+                                             size: CGSize(width: bounds.width, height: timelinePagerHeight))
+        }
+    }
+    
+    public func transitionToHorizontalSizeClass(_ sizeClass: UIUserInterfaceSizeClass) {
+        dayHeaderView.transitionToHorizontalSizeClass(sizeClass)
+        updateStyle(style)
+    }
+    
+    public func create(event: EventDescriptor, animated: Bool = false) {
+        timelinePagerView.create(event: event, animated: animated)
+    }
+    
+    public func beginEditing(event: EventDescriptor, animated: Bool = false) {
+        timelinePagerView.beginEditing(event: event, animated: animated)
+    }
+    
+    public func endEventEditing() {
+        timelinePagerView.endEventEditing()
+    }
+    
+        // MARK: TimelinePagerViewDelegate
+    
+    
+    public func timelinePagerDidChangeHeightScaleFactor(timelinePager: TimelinePagerView) {
+        delegate?.dayViewDidChangeHeightScaleFactor(dayView: self)
+    }
+    
+    public func timelinePagerDidSelectEventView(_ eventView: EventView) {
+        delegate?.dayViewDidSelectEventView(eventView)
+    }
+    public func timelinePagerDidLongPressEventView(_ eventView: EventView) {
+        delegate?.dayViewDidLongPressEventView(eventView)
+    }
+    public func timelinePagerDidBeginDragging(timelinePager: TimelinePagerView) {
+        delegate?.dayViewDidBeginDragging(dayView: self)
+    }
+    public func timelinePagerDidTransitionCancel(timelinePager: TimelinePagerView) {
+        delegate?.dayViewDidTransitionCancel(dayView: self)
+    }
+    public func timelinePager(timelinePager: TimelinePagerView, willMoveTo date: Date) {
+        delegate?.dayView(dayView: self, willMoveTo: date)
+    }
+    public func timelinePager(timelinePager: TimelinePagerView, didMoveTo  date: Date) {
+        delegate?.dayView(dayView: self, didMoveTo: date)
+    }
+    public func timelinePager(timelinePager: TimelinePagerView, didLongPressTimelineAt date: Date) {
+        delegate?.dayView(dayView: self, didLongPressTimelineAt: date)
+    }
+    public func timelinePager(timelinePager: TimelinePagerView, didTapTimelineAt date: Date) {
+        delegate?.dayView(dayView: self, didTapTimelineAt: date)
+    }
+    public func timelinePager(timelinePager: TimelinePagerView, didUpdate event: EventDescriptor) {
+        delegate?.dayView(dayView: self, didUpdate: event)
+    }
+    
+    public func openIntervalForDate(_ date: Date) -> NSDateInterval {
         
-      let allDayInterval = NSDateInterval.init(start: startOfToday, end: endOfToday)
+        let startOfToday = NSCalendar.current.startOfDay(for: date)
+        let endOfToday = Date(timeInterval: 86399, since: startOfToday)
         
-      return delegate?.openIntervalForDate( date) ?? allDayInterval
+        let allDayInterval = NSDateInterval.init(start: startOfToday, end: endOfToday)
+        
+        return delegate?.openIntervalForDate( date) ?? allDayInterval
     }
     
 }
--- Sources/Timeline/TimelineContainer.swift
+++ Sources/Timeline/TimelineContainer.swift
@@ -1,59 +1,115 @@
 import UIKit
 
 public final class TimelineContainer: UIScrollView {
-  public let timeline: TimelineView
-  
-  public init(_ timeline: TimelineView) {
-    self.timeline = timeline
-    super.init(frame: .zero)
-  }
-  
-  @available(*, unavailable)
-  required public init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override public func layoutSubviews() {
-    super.layoutSubviews()
-    timeline.frame = CGRect(x: 0, y: 0, width: bounds.width, height: timeline.fullHeight)
-    timeline.offsetAllDayView(by: contentOffset.y)
+    public let timeline: TimelineView
+    
+    public init(_ timeline: TimelineView) {
+        self.timeline = timeline
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        timeline.frame = CGRect(x: 0, y: 0, width: bounds.width, height: timeline.fullHeight)
+        timeline.offsetAllDayView(by: contentOffset.y)
+        
+        
+            //adjust the scroll insets
+        let allDayViewHeight = timeline.allDayViewHeight
+        let bottomSafeInset: CGFloat
+        if #available(iOS 11.0, *) {
+            bottomSafeInset = window?.safeAreaInsets.bottom ?? 0
+        } else {
+            bottomSafeInset = 0
+        }
+        scrollIndicatorInsets = UIEdgeInsets(top: allDayViewHeight, left: 0, bottom: bottomSafeInset, right: 0)
+        contentInset = UIEdgeInsets(top: allDayViewHeight, left: 0, bottom: bottomSafeInset, right: 0)
+    }
+    
+    public func prepareForReuse() {
+        timeline.prepareForReuse()
+    }
+    
+    public func scrollToFirstEvent(animated: Bool) {
+        let allDayViewHeight = timeline.allDayViewHeight
+        let padding = allDayViewHeight + 8
+        if let yToScroll = timeline.firstEventYPosition {
+            setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
+        }
+    }
+    
+    public func scrollTo(event: EventDescriptor?, animated: Bool) {
+        let allDayViewHeight = timeline.allDayViewHeight
+        let padding = allDayViewHeight + 8
+        if let yToScroll = timeline.yPosition(event: event) {
+            setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
+        }
+    }
+    
+    public func scrollTo(hour24: Float, animated: Bool = true) {
+        let percentToScroll = CGFloat(hour24 / 24)
+        let yToScroll = contentSize.height * percentToScroll
+        let padding: CGFloat = 8
+        setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
+    }
+    
+    public func scrollTo(hour24: Float, minute: Float, animated: Bool = true) {
+        let yearComponent = self.timeline.calendar.component(.year, from: self.timeline.date)
+        let monthComponent = self.timeline.calendar.component(.month, from: self.timeline.date)
+        let dayComponent = self.timeline.calendar.component(.day, from: self.timeline.date)
+        
+        let zone = self.timeline.calendar.timeZone
+        
+        var newComponents = DateComponents(timeZone: zone,
+                                           year: yearComponent,
+                                           month: monthComponent,
+                                           day: dayComponent)
+        newComponents.hour = Int(hour24)
+        newComponents.minute = Int(minute)
+        
+        guard let targetDate = self.timeline.calendar.date(from: newComponents) else {return}
+        
+        let allDayViewHeight = timeline.allDayViewHeight
+        let padding = allDayViewHeight + 8
+        let yToScroll = timeline.dateToY(targetDate)
+        setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
+    }
+    
+    public func scrollToNowTime(animated: Bool = true) {
+        let nowDate = Date()
+        let yearComponent = self.timeline.calendar.component(.year, from: self.timeline.date)
+        let monthComponent = self.timeline.calendar.component(.month, from: self.timeline.date)
+        let dayComponent = self.timeline.calendar.component(.day, from: self.timeline.date)
+        let hourComponent = self.timeline.calendar.component(.hour, from: nowDate)
+        let minuteComponent = self.timeline.calendar.component(.minute, from: nowDate)
+        
+        let zone = self.timeline.calendar.timeZone
+        
+        let newComponents = DateComponents(timeZone: zone,
+                                           year: yearComponent,
+                                           month: monthComponent,
+                                           day: dayComponent,
+                                           hour: hourComponent,
+                                           minute: minuteComponent)
+        
+        guard let targetDate = self.timeline.calendar.date(from: newComponents) else {return}
+        
+        let allDayViewHeight = timeline.allDayViewHeight
+        let padding = allDayViewHeight + 8
+        let yToScroll = timeline.dateToY(targetDate)
+        setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
+    }
     
     
-    //adjust the scroll insets
-    let allDayViewHeight = timeline.allDayViewHeight
-    let bottomSafeInset: CGFloat
-    if #available(iOS 11.0, *) {
-      bottomSafeInset = window?.safeAreaInsets.bottom ?? 0
-    } else {
-      bottomSafeInset = 0
+    private func setTimelineOffset(_ offset: CGPoint, animated: Bool) {
+        let yToScroll = offset.y
+        let bottomOfScrollView = contentSize.height - bounds.size.height
+        let newContentY = (yToScroll < bottomOfScrollView) ? yToScroll : bottomOfScrollView
+        setContentOffset(CGPoint(x: offset.x, y: newContentY), animated: animated)
     }
-    scrollIndicatorInsets = UIEdgeInsets(top: allDayViewHeight, left: 0, bottom: bottomSafeInset, right: 0)
-    contentInset = UIEdgeInsets(top: allDayViewHeight, left: 0, bottom: bottomSafeInset, right: 0)
-  }
-  
-  public func prepareForReuse() {
-    timeline.prepareForReuse()
-  }
-  
-  public func scrollToFirstEvent(animated: Bool) {
-    let allDayViewHeight = timeline.allDayViewHeight
-    let padding = allDayViewHeight + 8
-    if let yToScroll = timeline.firstEventYPosition {
-      setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
-    }
-  }
-  
-  public func scrollTo(hour24: Float, animated: Bool = true) {
-    let percentToScroll = CGFloat(hour24 / 24)
-    let yToScroll = contentSize.height * percentToScroll
-    let padding: CGFloat = 8
-    setTimelineOffset(CGPoint(x: contentOffset.x, y: yToScroll - padding), animated: animated)
-  }
-
-  private func setTimelineOffset(_ offset: CGPoint, animated: Bool) {
-    let yToScroll = offset.y
-    let bottomOfScrollView = contentSize.height - bounds.size.height
-    let newContentY = (yToScroll < bottomOfScrollView) ? yToScroll : bottomOfScrollView
-    setContentOffset(CGPoint(x: offset.x, y: newContentY), animated: animated)
-  }
 }
